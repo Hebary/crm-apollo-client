a6b3a5bbeb5960d78dfd362d223a348da3139773
Bernardo D'Addario <94189575+Khoderb@users.noreply.github.com>
2022-12-07 22:39:46 -0300

    Update login.tsx

--- pages/login.tsx
+++ pages/login.tsx
@@ -63,7 +63,7 @@ const Login : React.FC  = () : JSX.Element => {
                 setTimeout(() => {
                     router.push('/');
                 }
-                , 3000);
+                , 1500);
 
             } catch (error : any) {
                 setAlert( error.message.replace('GraphQL error: ', '') );
